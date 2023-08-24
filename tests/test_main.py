e4c2de4f5c3151db95352ec136670dd26f46ec91
Aaron Anderson <aaronanderson.co@gmail.com>
2023-08-24 14:36:18 -0600

    demo test

---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+import httpx
+from fastapi.testclient import TestClient
+from main import my_app
+
+
+client = TestClient(my_app)
+
+
+def test_root_endpoint():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World!!!"}
